9d1bb38c96ff225b2f267c9983dadbc977bb28c7
Wayne Viers <wviers@usgs.gov>
2011-12-14 17:16:48 -0600

    Changed code to fit google style guide.

--- map/views.py
+++ map/views.py
@@ -1,3 +1,7 @@
+"""
+USGS Rolla
+Wayne Viers
+"""
 from django.http import Http404
 from django.template import Context, loader
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
@@ -11,10 +15,44 @@ from string import Template
 
 
 def make_request(request, TRLo, TRLa, BRLo, BRLa, BLLo, BLLa, TLLo, TLLa):
+    """builds query, sends it, parses the response, and returns it.
+    
+    Recieves coordinates for a bounding rectangle, and builds those 
+    coordiantes into a sparql query.  Next the funciton parses the JSON
+    and returns the data as a JSON string.
+    
+    Arguments:
+        TRLo: The top right longitude.
+        TRLa: The top right latitude.
+        BRLo: The bottom right longitude.
+        BRLa: The bottom right latitude.
+        BLLo: The bottom left longitude.
+        BLLa: The bottom left latitude.
+        TLLo: The top left longitude.
+        TLLa: The top left latitude.
+        
+    Returns:
+        A JSON string containing the queried information.
+        
+    Raises:
+        None.
+    """
+    
     response = []
     return_list = []
     
-    query = 'PREFIX geo: <http://www.opengis.net/ont/OGC-GeoSPARQL/1.0/> PREFIX geo-sf: <http://www.opengis.net/def/dataType/OGC-SF/1.0/> PREFIX gn: <http://www.geonames.org/ontology#> SELECT DISTINCT ?school_name ?school_wkt WHERE { GRAPH <http://example.org/data> { LET (?place := "POLYGON((' + TRLo + " " + TRLa + ', ' + BRLo  + " " + BRLa + ', ' + BLLo + " " + BLLa + ', ' + TLLo + " " + TLLa + ", " + TRLo + " " + TRLa +'))"^^geo-sf:WKTLiteral) . ?school a gn:Feature ; geo:hasGeometry ?school_geo ; gn:featureCode gn:S.SCH . ?atl_geo a geo:Geometry ; geo:asWKT ?place . ?school_geo geo:sf-within ?atl_geo .  ?school_geo geo:asWKT ?school_wkt . ?school gn:name ?school_name. } }';
+    query = ('PREFIX geo: <http://www.opengis.net/ont/OGC-GeoSPARQL/1.0/> ' 
+             'PREFIX geo-sf: <http://www.opengis.net/def/dataType/OGC-SF/1.0/> '
+             'PREFIX gn: <http://www.geonames.org/ontology#> SELECT DISTINCT '
+             '?school_name ?school_wkt WHERE { GRAPH <http://example.org/data> '
+             '{ LET (?place := "POLYGON((' + TRLo + " " + TRLa + ', ' + BRLo 
+             + " " + BRLa + ', ' + BLLo + " " + BLLa + ', ' + TLLo + " " + TLLa
+             + ", " + TRLo + " " + TRLa +'))"^^geo-sf:WKTLiteral) . ?school a '
+             'gn:Feature ; geo:hasGeometry ?school_geo ; gn:featureCode gn:S.SCH '
+             '. ?atl_geo a geo:Geometry ; geo:asWKT ?place . ' 
+             '?school_geo geo:sf-within ?atl_geo .  '
+             '?school_geo geo:asWKT ?school_wkt . '
+             '?school gn:name ?school_name. } }')
 
     print query
     params = urllib.urlencode({'query':query, 'output':'json'})
@@ -24,8 +62,6 @@ def make_request(request, TRLo, TRLa, BRLo, BRLa, BLLo, BLLa, TLLo, TLLa):
     conn.request("POST", "/parliament/sparql", params, headers)
     r1 = conn.getresponse()
     print r1.status, r1.reason
-
-	
 	
     if r1.status == 200: 
         response = r1.read()
@@ -36,7 +72,6 @@ def make_request(request, TRLo, TRLa, BRLo, BRLa, BLLo, BLLa, TLLo, TLLa):
     elif (r1.status == 400):
         return "400"
 			
-
     conn.close
     
     items = len(data2['results']['bindings'])
@@ -48,8 +83,6 @@ def make_request(request, TRLo, TRLa, BRLo, BRLa, BLLo, BLLa, TLLo, TLLa):
     
     print return_list    
     return HttpResponse(json.dumps(return_list))
-
-
 
 
 def load_HTML(request):
